caa2edfef65d78d20c0ca9e604d84368cd912ba4
Zuby Afzal <git.zuby@gmail.com>
2025-05-13 19:23:57 -0700

    Add profanity checker

--- src/server/routes/chat.ts
+++ src/server/routes/chat.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import checkAuthentication from '../middleware/check-authentication';
 import chatMiddleware from '../middleware/chat';
+// @ts-ignore
+import * as filter from 'leo-profanity';
 
 const router = express.Router();
 
@@ -12,10 +14,15 @@ router.get('/:roomId', checkAuthentication, chatMiddleware, (request, response) 
 
 router.post('/:roomId', checkAuthentication, chatMiddleware, (request, response) => {
   const { roomId } = request.params;
-  const message = request.body.message;
+  let message = request.body.message;
   // @ts-expect-error
   const { email, gravatar, username } = request.session.user;
   const io = request.app.get('io');
+
+  if (filter.check(message)) {
+    response.status(400).json({ error: 'Inappropriate language not allowed' });
+    return;
+  }
 
   io.emit(`message:${roomId}`, {
     message,
