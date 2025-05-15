cbde01f54d9dc90976740e91704d351d2fe90947
Zuby Afzal <git.zuby@gmail.com>
2025-05-15 10:56:27 -0700

    Apply Prettier formatting

--- src/server/db/connection.ts
+++ src/server/db/connection.ts
@@ -7,14 +7,14 @@ const pgp = pgPromise();
 const isProduction = process.env.NODE_ENV === 'production';
 
 const db = pgp(
-    isProduction
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false },
-        }
-        : {
-            connectionString: process.env.DATABASE_URL,
-        }
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        connectionString: process.env.DATABASE_URL,
+      },
 );
 
 export default db;
